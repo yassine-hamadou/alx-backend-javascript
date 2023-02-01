14ef21d59d9aba9e736084ee12b459e8dc307a5e
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-02-01 00:11:55 +0000

    Added update

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(array) {
-    return array.reduce((accumulator, i) => accumulator + i.id)
+    return array.reduce((accumulator, i) => accumulator + i.id, 0);
 };
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,3 @@
+export default function updateStudentGradeByCity(array, city, newGrades) {
+
+}
