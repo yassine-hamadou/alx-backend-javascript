fabe95c61890a5eb293cd61b46ec4c6cc3e6412d
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-02-02 01:46:28 +0000

    Add any attribute to the Object

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,87 +1,51 @@
-export interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
     location: string;
+    [index:string]: any;
 }
-const studentA: Student = {
-    firstName: "Harry",
-    lastName: "Yoon",
-    age: 22,
-    location: "Seoul, South Korea",
-};
-const studentB: Student = {
-    firstName: "Anjali",
-    lastName: "Prajapati",
-    age: 22,
-    location: "Lodz, Poland",
-};
 
-const studentsList: Array<Student> = [
-    studentA,
-    studentB,
-];
-const styleSheet = `
-  html {
-    margin: 0;
-    height: 100%;
-  }
-  body {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80%;
-    margin: 10%;
-  }
-  table {
-    border-collapse: collapse;
-  }
-  thead {
-    font-weight: bold;
-  }
-  td {
-    padding: 10px;
-    border: 1px solid gray;
-    cursor: pointer;
-  }
-  td:hover {
-    background: gainsboro;
-  }
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
 
-  td:nth-child(1) {
-    text-align: center;
-  }
-`;
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
 
-/**
- * Displays information about students in a table.
- * @param students The list of students to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
-export const displayStudents = (students: Array<Student>): void => {
-    const table = document.createElement('table');
-    const tableHead = document.createElement('thead');
-    const headRow = document.createElement('tr');
-    const tableBody = document.createElement('tbody');
-    headRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
-    headRow.insertAdjacentHTML('beforeend', '<td>Location</td');
-    tableHead.insertAdjacentElement('beforeend', headRow);
+export function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
 
-    for (const student of students) {
-        const bodyRow = document.createElement('tr');
-        bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
-        bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
-        tableBody.insertAdjacentElement('beforeend', bodyRow);
+export interface IStudentClassConstructor {
+    new (firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+    private _firstName!: string;
+    private _lastName!: string;
+
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
     }
 
-    table.insertAdjacentElement('beforeend', tableHead);
-    table.insertAdjacentElement('beforeend', tableBody);
-    document.body.insertAdjacentElement('beforeend', table);
-};
+    workOnHomework() {
+        return 'Currently working';
+    }
 
-displayStudents(studentsList);
-const styleSheetElement = document.createElement('style');
-styleSheetElement.innerHTML = styleSheet;
-document.head.insertAdjacentElement('beforeend', styleSheetElement);
-document.title = 'Task 0';
+    displayName() {
+        return this._firstName;
+    }
+}
+
+export function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+    return new ctor(firstName, lastName);
+}
